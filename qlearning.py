d37ce38e09d58e5adcbb27467d56ab7c8bfd51f2
Robert Stefanic <rstefanic72@gmail.com>
2022-12-28 14:41:54 -0500

    WIP

--- qlearning.py
+++ qlearning.py
@@ -11,7 +11,7 @@ def get_qtable_columns():
 
 def build_q_table():
     q_table = []
-    for i in range(0, 3):
+    for _ in range(0, 3):
         q_table.append([0.0 for _ in flatten(get_qtable_columns())])
 
     return q_table
@@ -19,27 +19,36 @@ def build_q_table():
 
 def make_random_choice(roll, board):
     choices = get_possibilities(roll)
+    valid_choices = []
     for choice in choices:
-        if choice not in board:
-            choices.remove(choice)
-    return choices[random.randint(0, len(choices) - 1)]
+        if all(elem in board for elem in choice):
+            valid_choices.append(choice)
+    return valid_choices[random.randint(0, len(valid_choices) - 1)]
 
 
 def main():
     qtable = build_q_table()
+    columns = flatten(get_qtable_columns())
     game = Game()
     while not game.get_game_over():
-        print(game)
-
         while True:
             try:
-                boxes_to_shut = make_random_choice(game.get_roll(), game.get_board())
-                game.shut_boxes(boxes_to_shut)
+                choice = make_random_choice(game.get_roll(), game.get_board().state)
+                game.shut_boxes(choice)
                 break
             except ValueError as err:
                 print(err)
 
         game.tick()
+
+        column_index = columns.index(choice)
+
+        if game.get_game_over() and game.get_score() == 0: # Winner
+            qtable[1][column_index] = game.get_score()
+        elif game.get_game_over(): # Record Loss
+            qtable[2][column_index] = game.get_score()
+        else: # Keep Going
+            qtable[0][column_index] = game.get_score()
 
     print("\nGame over. Final score: " + str(game.get_score()))
 
